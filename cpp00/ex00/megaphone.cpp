c7b2b6b81d25b4cf6bf32052eec7ba837225092e
Fahima BOUMELLIL <fboumell@e3r5p11.clusters.42paris.fr>
2022-06-09 18:16:50 +0200

    a

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/09 14:52:35 by fboumell          #+#    #+#             */
+/*   Updated: 2022/06/09 18:15:13 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <string>
 
-using namespace std;
-
-int main()
+int main(int ac, char **av)
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	(void)av;
+	if (ac < 2)
+		std:: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std:: endl;
+	return (0);
 }
