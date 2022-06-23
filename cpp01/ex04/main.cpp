09201d1438971e2eb76ae91516182df39ddb79b5
Fahima BOUMELLIL <fboumell@e3r1p3.42.fr>
2022-06-23 17:15:43 +0200

    cpp01/ex04 ongoing

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 14:28:18 by fboumell          #+#    #+#             */
-/*   Updated: 2022/06/22 13:51:34 by fboumell         ###   ########.fr       */
+/*   Updated: 2022/06/23 16:06:24 by fboumell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,28 @@ int	main(int ac, char **av)
 		file1.open(av[1]);
 		if (file1.is_open())
 		{
+			std::getline(file1, line);
+			if (file1.eof())
+			{
+				file1.close();
+				std::cout << "The file " << av[1] << " is empty" << std::endl;
+			}
 			str_replace = av[1] + std::string(".replace");
 			replace.open(str_replace.c_str(), std::ofstream::out);
 			if (replace.is_open())
 			{
-				file1 >> line;
-				// std::cout << "line = " << line << std::endl;
 				while (!file1.eof())
 				{
-					replace << line << std::endl;
-					file1 >> line;
+					replace << line << "\n";
+					std::getline(file1, line);
 				}
 			}
 			else
-				std::cout << "File.replace can not be open" << std::endl;
+				std::cout << str_replace << " can not be open" << std::endl;
 			replace.close();
 		}
 		else
-			std::cout << "File can not be open" << std::endl;
+			std::cout << av[1] << " can not be open" << std::endl;
 		file1.close();
 	}
 	return (0);
