a4e31704dbaea5278c0efa0f66434367519002e8
Fahima BOUMELLIL <fboumell@e3r12p1.42.fr>
2022-06-30 15:58:04 +0200

    cpp02/ex01 done

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 11:40:48 by fboumell          #+#    #+#             */
+/*   Updated: 2022/06/30 15:56:45 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int entierConst)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_entier = entierConst << this->_entierConst;
+}
+
+Fixed::Fixed(const float floatConst)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_entier = roundf(floatConst * (float)(1 << this->_entierConst));
+}
+
+Fixed::Fixed(const Fixed &nb)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = nb;
+}
+
+Fixed	&Fixed::operator=(const Fixed &nb)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_entier = nb.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->_entier);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_entier = raw;
+}
+
+int		Fixed::toInt() const 
+{
+	return (this->_entier >> this->_entierConst);
+}
+
+float	Fixed::toFloat() const
+{
+	return ((float) this->_entier / (float)(1 << this->_entierConst));
+}
+
+std::ostream	&operator<<(std::ostream &flux, Fixed const &nb)
+{
+	flux << nb.toFloat();
+	return (flux);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 11:39:51 by fboumell          #+#    #+#             */
+/*   Updated: 2022/06/30 15:54:47 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+
+
+class Fixed
+{
+private:
+
+	int _entier;
+	static const int	_entierConst = 8;
+	
+public:
+
+	/*  constructeur & destructeur   */
+		Fixed();
+		Fixed(const int entierConst);
+		Fixed(const float floatConst);
+		~Fixed();
+	
+	/*	constructeur de recopie	*/
+		Fixed(const Fixed &nb_entier);
+		
+	/*	operateur d'affectation	*/
+		Fixed	&operator = (const Fixed &nb);	
+	
+	/*	Methods	*/
+		int 	getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &flux, Fixed const &nb);
+
+#endif
