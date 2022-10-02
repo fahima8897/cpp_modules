50c44b71cda2f141515b28ef8100bfaa5644e0d5
Fahima BOUMELLIL <fboumell@e2r2p1.clusters.42paris.fr>
2022-10-02 20:38:05 +0200

    cpp08/ex00 ongoing

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fboumell <fboumell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/02 17:48:03 by fboumell          #+#    #+#             */
+/*   Updated: 2022/10/02 20:36:40 by fboumell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASY_FIND_HPP
+# define EASY_FIND_HPP
+
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+
+class NotFoundException : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return ("Occurence not found!");
+    }
+};
+
+template<typename T>
+typename T::iterator easyfin(T &container, int n)
+{
+    typename T::iterator it;
+    
+    it = find(container.begin(), container.end(), n);
+    if ()
+}
+
+#endif
